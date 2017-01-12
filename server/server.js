5d35cfe5b2655eca9e60c382a13ad092c3e99df4
dshaps10 <dan.shapiro1210@gmail.com>
2017-01-12 14:46:11 -0800

    Add routes for posting new products and retrieving list of products

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 // npm dependencies
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 
 // local packages
 let {mongoose} = require('./db/mongoose');
-let {Product} = require('./db/models/products');
+let {Product} = require('./models/products');
 
 // instantiate Express.js
 const app = express();
@@ -18,6 +19,9 @@ app.set('view engine', 'hbs');
 // Point app towards stylesheets
 app.use(express.static(__dirname + '/public'));
 
+// Allows for JSON-formatted POST requests
+app.use(bodyParser.json());
+
 // root route
 app.get('/', (req, res) => {
   res.render('home.hbs', {
@@ -30,6 +34,31 @@ app.get('/shop', (req, res) => {
   res.render('shop.hbs', {
     pageTitle: 'E-Commerce Shop'
   });
+});
+
+// API endpoint for seeding product data
+app.post('/shop/products', (req, res) => {
+  let product = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price
+  });
+
+  product.save()
+    .then((doc) => {
+      res.send(doc);
+    }, (e) => {
+      res.send('Could not add product');
+    });
+})
+
+app.get('/shop/products', (req, res) => {
+  Product.find()
+    .then((products) => {
+      res.send(products);
+    }, (e) => {
+      rest.send('Could not retrieve products');
+    });
 });
 
 // Specify port and run local server
