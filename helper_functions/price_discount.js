e65525a563d93089515289359d5ad5a858528105
dshaps10 <dan.shapiro1210@gmail.com>
2017-01-18 14:47:43 -0800

    Add script to truncate discounted prices in 'priceDiscount()' function

--- helper_functions/price_discount.js
+++ helper_functions/price_discount.js
@@ -2,7 +2,8 @@
 // Discounts sale price by 15% is price is > $8,000
 module.exports.priceDiscount = (price) => {
 	if (price > 8000) {
-		return price / 1.15
+		let discountedPrice = price / 1.15
+		return Math.floor(discountedPrice);
 	}
 	return price;
 };
