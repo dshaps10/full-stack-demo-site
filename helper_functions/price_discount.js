a8ad29163c1021ccc7055f06745d8b1a43623f3e
dshaps10 <dan.shapiro1210@gmail.com>
2017-01-18 13:12:59 -0800

    Fix function so it properly deducts 15% from price

--- helper_functions/price_discount.js
+++ helper_functions/price_discount.js
@@ -2,7 +2,7 @@
 // Discounts sale price by 15% is price is > $8,000
 module.exports.priceDiscount = (price) => {
 	if (price > 8000) {
-		return price * .15
+		return price / 1.15
 	}
 	return price;
 };
