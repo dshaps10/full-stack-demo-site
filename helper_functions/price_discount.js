d2fec9523af42edc2d620da53a1c9eaf20c9efae
dshaps10 <dan.shapiro1210@gmail.com>
2017-01-18 13:04:54 -0800

    Modify 'priceDiscount()' function to reduce price by 15% if sale price only greater than 8000

--- helper_functions/price_discount.js
+++ helper_functions/price_discount.js
@@ -1,3 +1,8 @@
+// priceDiscount function
+// Discounts sale price by 15% is price is > $8,000
 module.exports.priceDiscount = (price) => {
-	return price * .30;
-}
+	if (price > 8000) {
+		return price * .15
+	}
+	return price;
+};
