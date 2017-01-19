88c96eb22e4627c02999096babbc71d839a6956c
dshaps10 <dan.shapiro1210@gmail.com>
2017-01-19 15:48:22 -0800

    Add photos and pricing to search results page

--- helpers/shop-seed.js
+++ helpers/shop-seed.js
@@ -5,17 +5,17 @@ let {mongoose} = require('../db/mongoose');
 // collection of documents pertaining to Product models
 let products = [
   {
-    "img": "http://www.motorcycle-usa.com/wp-content/uploads/2016/09/H-D-FatBoyLo-2016.jpg?378220",
+    "img": "http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-Blue-Harley-Davidson-Motorcycle-Bike-Side-View-PNG-Image.png",
     "title": "1968 Harley Davidson",
     "description": "An oldie but a goodie!",
     "price": "10000"
   }, {
-    "img": "http://www.totalmotorcycle.com/motorcycles/2012models/2012-Triumph-Thruxton6.jpg",
+    "img": "http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-Triumph-Rocket-III-Motorcycle-Bike-PNG-Image.png",
     "title": "2006 Triumph",
     "description": "A new riff on a classic with some stylish looks",
     "price": "6000"
   }, {
-    "img": "http://www.motorcycle-usa.com/wp-content/uploads/2015/10/2016_Honda_VFR1200X.jpg?378220",
+    "img": "http://pngimg.com/upload/motorcycle_PNG3132.png",
     "title": "1998 Honda",
     "description": "A mean racing bike",
     "price": "4400"
@@ -25,32 +25,32 @@ let products = [
     "description": "Recently received an upgrade and is ready for a new, loving owner",
     "price": "6500"
   }, {
-    "img": "http://www.totalmotorcycle.com/photos/2011models/2011-Honda-ShadowRS-VT750RSa.jpg",
+    "img": "http://www.motorcyclemag.com.ph/wp-content/uploads/2015/09/Honda-Supremo-Image-19885.png",
     "title": "1986 Honda",
     "description": "This one could use a little work, but is still a great bike",
     "price": "2000"
   }, {
-    "img": "http://images.motorcycle-usa.com/PhotoGallerys/15-Moto-Guzzi_eldorado.jpg",
-    "title": "2011 Moto Guzzi",
-    "description": "This bike was ridden very lightly and is ready to be taken out on the road again",
-    "price": "12000"
+    "img": "http://www.granstarcorp.com/dev/files/12062013401702820.png",
+    "title": "2011 Vespa",
+    "description": "An amazingly speedy scooter that's great for getting around the city",
+    "price": "3000"
   }, {
-    "img": "http://www.motorcyclistonline.com/sites/motorcyclistonline.com/files/styles/large_1x_/public/images/2016/01/triumph-bonneville-08.jpg?itok=FyVoYRm4",
+    "img": "http://www.dusejamoto.com/wp-content/uploads/photo-gallery/Triumph/Triumph%20Scrambler/.original/MG6_MY15_Scrambler_PG_LHS.png",
     "title": "2002 Triumph",
     "description": "The ideal bike for someone learning to ride for the first time",
     "price": "8200"
   }, {
-    "img": "https://s-media-cache-ak0.pinimg.com/originals/26/8a/36/268a36474c8543c7da55ff91bd943052.jpg",
+    "img": "http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-BMW-R1200GS-Adventure-Motorcycle-Bike-Side-PNG-Image.png",
     "title": "2007 BMW",
     "description": "A classic European sport bike that really flies",
     "price": "14000"
   }, {
-    "img": "http://www.motorcycle-usa.com/wp-content/uploads/2016/11/Duc-959-Panigale-2016.jpg",
-    "title": "2016 Ducatti",
-    "description": "A beautiful scrambler that handles well both on and offroad",
+    "img": "http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-Yellow-Ducati-Scrambler-Motorcycle-Bike-PNG-Image.png",
+    "title": "2016 Ducati",
+    "description": "A beautiful scrambler that handles well both on and off-road",
     "price": "13300"
   }, {
-    "img": "http://www.motorcycle-usa.com/wp-content/uploads/2015/12/2016_KTM450SX-F_FactoryEdition-02.jpg?378220",
+    "img": "http://www.pngpix.com/wp-content/uploads/2016/07/PNGPIX-COM-KTM-125-Duke-Sports-Motorcycle-Bike-PNG-Image.png",
     "title": "2014 KTM",
     "description": "A zippy little bike that's a ton of bang for your buck",
     "price": "6500"
@@ -68,5 +68,4 @@ Product.insertMany(products)
     console.log(docs);
   }, (e) => {
     console.log('Unable to seed data', e);
-  })
-  .end();
+  });
