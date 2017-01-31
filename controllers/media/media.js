457bb210bdac79b174dc98acd40e712c235e78da
dshaps10 <dan.shapiro1210@gmail.com>
2017-01-31 13:15:10 -0800

    Implement 'SAMPLE_ARTICLE_EXPERIMENT'

--- controllers/media/media.js
+++ controllers/media/media.js
@@ -32,16 +32,92 @@ const media = express();
 // });
 
 media.get('/media', (req, res) => {
-    Article.find().sort({"category": -1})
-        .then((articles) => {
-            res.render('media/home.hbs', {
-            	mainArticle: articles[0],
-            	topStories: articles.slice(1, 7),
-                samples: articles.slice(8, 11)
+    let id = req.params.id;
+
+    // generate random userID
+    userID = uuid();
+
+    // Activate Optimizely experiment
+    let variation = optimizelyClient.activate("SAMPLE_ARTICLE_EXPERIMENT", userID);
+
+
+    if (variation === 'original') {
+        Article.find().sort({"category": -1})
+            .then((articles) => {
+                res.render('media/home.hbs', {
+                    mainArticle: articles[0],
+                    topStories: articles.slice(1, 7),
+                    samples: articles.slice(8, 11)
+                    // samples: sampleArticleByCategory(article, 'Politics')
+                });
+            }, (e) => {
+                res.send('Could not retrieve articles');
             });
-        }, (e) => {
-            res.send('Could not retrieve articles');
-        });
+    } else if (variation === 'politics') {
+        Article.find().sort({"category": -1})
+            .then((articles) => {
+                res.render('media/home.hbs', {
+                	mainArticle: articles[0],
+                	topStories: articles.slice(1, 7),
+                    samples: articles.filter((article) => { return article.category === 'Politics' })
+                    // samples: sampleArticleByCategory(article, 'Politics')
+                });
+            }, (e) => {
+                res.send('Could not retrieve articles');
+            });
+    } else if (variation === 'life') {
+        Article.find().sort({"category": -1})
+            .then((articles) => {
+                res.render('media/home.hbs', {
+                    mainArticle: articles[0],
+                    topStories: articles.slice(1, 7),
+                    samples: articles.filter((article) => { return article.category === 'Life' })
+                    // samples: sampleArticleByCategory(article, 'Politics')
+                });
+            }, (e) => {
+                res.send('Could not retrieve articles');
+            });
+
+    } else if (variation === 'news') {
+        Article.find().sort({"category": -1})
+            .then((articles) => {
+                res.render('media/home.hbs', {
+                    mainArticle: articles[0],
+                    topStories: articles.slice(1, 7),
+                    samples: articles.filter((article) => { return article.category === 'News' })
+                    // samples: sampleArticleByCategory(article, 'Politics')
+                });
+            }, (e) => {
+                res.send('Could not retrieve articles');
+            });
+
+    } else if (variation === 'sports') {
+        Article.find().sort({"category": -1})
+            .then((articles) => {
+                res.render('media/home.hbs', {
+                    mainArticle: articles[0],
+                    topStories: articles.slice(1, 7),
+                    samples: articles.filter((article) => { return article.category === 'Sports' })
+                    // samples: sampleArticleByCategory(article, 'Politics')
+                });
+            }, (e) => {
+                res.send('Could not retrieve articles');
+            });
+        
+    } else {
+        Article.find().sort({"category": -1})
+            .then((articles) => {
+                res.render('media/home.hbs', {
+                    mainArticle: articles[0],
+                    topStories: articles.slice(1, 7),
+                    samples: articles.slice(8, 11)
+                    // samples: sampleArticleByCategory(article, 'Politics')
+                });
+            }, (e) => {
+                res.send('Could not retrieve articles');
+            });
+    }
+
 });
 
 module.exports = {
--- helpers/sampleArticleByCategory.js
+++ helpers/sampleArticleByCategory.js
@@ -2,17 +2,8 @@
 let {mongoose} = require('./../db/mongoose');
 let {Article} = require('./../models/articles');
 
-module.exports.sampleArticleByCategory = (category) => {
-	console.log('STARTING TO RUN BITCHES');
-	var sampleArticles;
-	Article.find({'category': category})
-		.then((articles) => {
-			sampleArticles = articles;
-			console.log('Sample articles helper running');
-			console.log(articles.slice(0, 3));
-			return sampleArticles;
-		}, (e) => {
-			console.log('could not retrieve articles', e);
-		});
-	console.log('HIII IM DONE RUNNING!');
+module.exports.sampleArticleByCategory = (collection, category) => {
+	collection.filter((article) => {
+		return article.category === category
+	})	
 };
